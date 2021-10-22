de57b0b30ea0537d2302cdf20195b27c047e3a8d
lills1 <lilly.stephenson4@gmail.com>
2021-10-22 11:45:06 +1100

    got rid of unnecessary comments

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -15,14 +15,6 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) { }
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) { }
-
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
@@ -31,7 +23,6 @@ function generateMarkdown(data) {
   ## Description
   ${data.description}
   ## Table of Contents 
-  If your README is long, add a table of contents to make it easy for users to find what they need.
   - [Installation](#installation)
   - [Usage](#usage)
   - [License](#license)
